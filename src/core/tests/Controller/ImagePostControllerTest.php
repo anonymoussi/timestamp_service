6cac5ce902803e3ecd303edb4970405b7cad0b94
andy-si <andrew.sinukov.it@gmail.com>
2020-03-10 10:38:47 +0200

    added web tests

--- src/core/tests/Controller/ImagePostControllerTest.php
+++ src/core/tests/Controller/ImagePostControllerTest.php
@@ -3,8 +3,32 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Messenger\Transport\InMemoryTransport;
 
 class ImagePostControllerTest extends WebTestCase
 {
+    public function testOne()
+    {
+        $client = static::createClient();
 
+        $uploadedFile = new UploadedFile(
+            __DIR__.'/../Fixtures/glow.jpg',
+            'glow.jpg'
+        );
+
+        $client->request(
+            'POST',
+            '/api/images',
+            [],
+            ['file' => $uploadedFile]
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        /** @var InMemoryTransport $transposrt*/
+        $transposrt = self::$container->get('messenger.transport.async_priority_high');
+
+        $this->assertCount(1, $transposrt->get());
+    }
 }
